d4ca182041e00d30c42820b19fe6d69b6bceee1b
ijuyong <ijuyong@gmail.com>
2023-03-21 23:01:52 +0900

    21강 완료

---
+++ src/bubble/test/ex15/BubbleFrame.java
@@ -0,0 +1,91 @@
+package bubble.test.ex15;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BubbleFrame extends JFrame {
+
+	private BubbleFrame mContext = this;
+	private JLabel backgroundMap;
+	private Player player;
+	private Enemy enemy;
+
+	public BubbleFrame() {
+		initObject();
+		initSetting();
+		initListener();
+		setVisible(true);
+	}
+
+	private void initObject() {
+		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+		setContentPane(backgroundMap);
+		player = new Player(mContext);
+		add(player);
+		enemy = new Enemy(mContext);
+		add(enemy);
+	}
+
+	private void initSetting() {
+		setSize(1000, 640);
+		setLayout(null); // absolute layout (자유롭게 그리기)
+		setLocationRelativeTo(null); // JFrame을 가운데에 위치
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // x버튼으로 JVM 같이 종료
+	}
+
+	private void initListener() {
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if(!player.isLeft() && !player.isLeftWallCrash()) {
+						player.left();						
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(!player.isRight() && !player.isRightWallCrash()) {
+						player.right();						
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(!player.isUp() && !player.isDown()) {
+						player.up();						
+					}
+					break;
+				case KeyEvent.VK_SPACE:
+					//Bubble bubble = new Bubble(mContext);
+					//add(bubble);
+					player.attack();
+					break;
+				}
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				}
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+};;
